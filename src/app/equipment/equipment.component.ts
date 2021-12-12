530192951c62cab07723395758d436cc272d43f0
Sergio Estrada <estrada.serg@gmail.com>
2021-12-12 08:39:14 -0600

    completed all tasks

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  /*<ul>
+  <li>Habitat dome</li>
+  <li>Drones</li>
+  <li>Food containers</li>
+  <li>Oxygen tanks</li>
+</ul>*/
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  equipmentBeingEdited: object = null;
+
+  equipment: object[] = [
+    {name: "Habitat dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen Tanks"}
+  ]
+
+  add(equipmentName: string) {
+    if(this.equipment.find(x => x["name"].toLowerCase() === equipmentName.toLowerCase()) === undefined){
+      this.equipment.push({name: equipmentName});
+    }
+  }
+
+  remove(equipment: object) {
+    let index = this.equipment.indexOf(equipment);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(equipment: object) {
+    this.equipmentBeingEdited = equipment;
+ }
+
+ save(name: string, equipment: object) {
+  equipment['name'] = name;
+  this.equipmentBeingEdited = null;
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+/*
+<ul>
+  <li>Mars soil sample</li>
+  <li>Plant growth in habitat</li>
+  <li>Human bone density</li>
+</ul>*/
   constructor() { }
 
   ngOnInit() {
+  }
+
+  experimentBeingEdited: object = null;
+
+  experiments: object[] = [
+    {name: "Mars soil sample"},
+    {name: "Planet growth in habitat"},
+    {name: "Human bone density"}
+  ]
+
+  add(experimentName: string) {
+    if(this.experiments.find(x => x["name"].toLowerCase() === experimentName.toLowerCase()) === undefined){
+      this.experiments.push({name: experimentName});
+    }
+  }
+
+  remove(experiment: object) {
+    let index = this.experiments.indexOf(experiment);
+    this.experiments.splice(index, 1);
+  }
+
+  edit(experiment: object) {
+    this.experimentBeingEdited = experiment;
+ }
+
+ save(name: string, experiment: object) {
+  experiment['name'] = name;
+  this.experimentBeingEdited = null;
   }
 
 }
